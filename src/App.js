ac83bcf4ca8f31913b344d497c7300fc7a05fd2f
pic301 <ppic301@gmail.com>
2020-03-02 20:33:33 +0900

    bootstrap 를 사용해서 챔피언 사진마다 Grid 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import { Row , Col, Container } from 'react-bootstrap';
 
 
 const App = () => {
@@ -9,20 +10,25 @@ const App = () => {
   axios.get('http://ddragon.leagueoflegends.com/cdn/10.4.1/data/ko_KR/champion.json')
   .then(res => 
       setChampion(res.data.data)
-    )
-    
+    ).catch(err => console.log(err))
  },[])
     console.log(champion)
 
   const realChampion = Object.keys(champion).map((cham) => champion[cham])
-  console.log("진짜", realChampion)
+  console.log(realChampion)
   return (
-    <div>
-     {realChampion.map(cham => <div>
-      <img src={`http://ddragon.leagueoflegends.com/cdn/10.4.1/img/champion/${cham.image.full}`} alt=""/>
-      <p>{cham.name}</p>
-     </div>)}
-    </div>
+    <Container>
+        <Row>
+          {realChampion.map(cham => 
+             <Col xs={4} md={2}>
+             <img src={`http://ddragon.leagueoflegends.com/cdn/10.4.1/img/champion/${cham.image.full}`} alt=""/>
+             <p>{cham.name}</p>
+             </Col>
+          )}
+      </Row>
+    </Container>
+   
+   
   );
 };
 
