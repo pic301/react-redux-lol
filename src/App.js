1a9643ead4b32299ae409e21683677ae7305e849
pic301 <ppic301@gmail.com>
2020-03-05 21:24:19 +0900

    redux 준비
    스토어  루트 리듀서 추가

--- src/App.js
+++ src/App.js
@@ -1,83 +1,114 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import { Row , Col, Container } from 'react-bootstrap';
-import Image from 'react-bootstrap/Image'
-import Card from 'react-bootstrap/Card'
-import ProgressBar from 'react-bootstrap/ProgressBar'
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
-import CircleImage from './components/circleImage'
-import styled from 'styled-components'
-import fireWater from './images/fire-and-water-2354583_1920.jpg'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import FistImage from "./components/fistImage";
+
+import { Row, Col, Container,Nav } from "react-bootstrap";
+import Image from "react-bootstrap/Image";
+import Card from "react-bootstrap/Card";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import CircleImage from "./components/circleImage";
 
 const StyledContainer = styled(Container)`
-  margin-Bottom:500px; 
-  border:3px solid blue;
-  color:#ffffff;
+  margin-bottom: 500px;
+  border: 3px solid blue;
+  color: #ffffff;
 `;
 
 const Wrapper = styled.div`
-  display:flex;
-  background-color:#292929;
+  display: flex;
+  background-color: #292929;
 `;
 const StyledImage = styled(Image)`
   cursor: pointer;
 `;
 
-const baseURL = "http://ddragon.leagueoflegends.com/cdn/10.4.1"
+const baseURL = "http://ddragon.leagueoflegends.com/cdn/10.4.1";
 
 const App = () => {
-  const [champion ,setChampion] = useState('')
+  const [champion, setChampion] = useState("");
 
- useEffect(()=>{
-  axios.get(`${baseURL}/data/ko_KR/champion.json`)
-  .then(res => 
-      setChampion(res.data.data)
-    ).catch(err => console.log(err))
-    
+  useEffect(() => {
+    axios
+      .get(`${baseURL}/data/ko_KR/champion.json`)
+      .then(res => setChampion(res.data.data))
+      .catch(err => console.log(err));
+  }, []);
+  console.log(champion);
 
- },[])
-    console.log(champion)
+  const realChampion = Object.keys(champion).map(cham => champion[cham]);
+  console.log(realChampion);
 
-  const realChampion = Object.keys(champion).map((cham) => champion[cham])
-  console.log(realChampion)
-
-  const now = 100
+  const now = 100;
 
   return (
-  <div style={{backgroundColor:"#333333"}}>
-    <StyledContainer >
-    <Image width="100%" height="500px" src={fireWater}></Image>
-     <Wrapper >
-        <Row >
-          {realChampion.map(cham => 
-             <Col xs={6} sm={4} md={2} key={cham.key}>
-               <OverlayTrigger  placement="bottom"  overlay={<Card style={{ width: '18rem' , padding:"1px"}}>
-                <Card.Img variant="top"  src={`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${cham.id}_0.jpg`} />
-                  <Card.Body>
-                    <Card.Title >{cham.name}</Card.Title>
-                    <Card.Text>
-                    {cham.title}
-                    </Card.Text>
-                    <Card.Text>
-                    <ProgressBar variant="success" now={now} label={`${now}%`} />
-                    {`HP:${cham.stats.hp}`}
-                    {`/MP:${cham.stats.mp}`}
-                  </Card.Text>
-                </Card.Body>
-              </Card>}>
-              <StyledImage roundedCircle src={`${baseURL}/img/champion/${cham.image.full}`} alt="champion"/>
-              </OverlayTrigger>
-               <p style={{textAlign:"center" ,paddingRight:"17px"}}>{cham.name}</p>
-             </Col>
-          )}
-      </Row>
-    <CircleImage/>
-   </Wrapper>
-    </StyledContainer>
- 
-  </div>
-
-   
+    <div style={{ backgroundColor: "#333333" }}>
+      <Nav
+  activeKey="/home"
+  onSelect={selectedKey => alert(`selected ${selectedKey}`)}
+>
+  <Nav.Item>
+    <Nav.Link href="/home">Active</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-1">Link</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-2">Link</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="disabled" disabled>
+      Disabled
+    </Nav.Link>
+  </Nav.Item>
+</Nav>
+      <StyledContainer>
+        <FistImage />
+        <Wrapper>
+          <Row>
+            {realChampion.map(cham => (
+              <Col xs={6} sm={4} md={2} key={cham.key}>
+                <OverlayTrigger
+                  placement="bottom"
+                  overlay={
+                    <Card style={{ width: "18rem", padding: "1px" }}>
+                      <Card.Img
+                        variant="top"
+                        src={`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${cham.id}_0.jpg`}
+                      />
+                      <Card.Body>
+                        <Card.Title>{cham.name}</Card.Title>
+                        <Card.Text>{cham.title}</Card.Text>
+                        <Card.Text>
+                          <ProgressBar
+                            variant="success"
+                            now={now}
+                            label={`${now}%`}
+                          />
+                          {`HP:${cham.stats.hp}`}
+                          {`/MP:${cham.stats.mp}`}
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  }
+                >
+                  <StyledImage
+                    roundedCircle
+                    src={`${baseURL}/img/champion/${cham.image.full}`}
+                    alt="champion"
+                  />
+                </OverlayTrigger>
+                <p style={{ textAlign: "center", paddingRight: "17px" }}>
+                  {cham.name}
+                </p>
+              </Col>
+            ))}
+          </Row>
+          <CircleImage />
+        </Wrapper>
+      </StyledContainer>
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./modules";
 
+const store = createStore(rootReducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
