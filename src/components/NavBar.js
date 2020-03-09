0c447d6bd84ba02698408493b453b9d66759455d
pic301 <ppic301@gmail.com>
2020-03-10 02:05:24 +0900

    etc 커밋

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import { Nav} from "react-bootstrap";
+import { FiShoppingCart} from "react-icons/fi";
 
 const NavBar = () => {
     return (
         <Nav activeKey="/home" onSelect style={{backgroundColor:"#ffffff"}}>
-        <Nav.Item>
-          <Nav.Link href="/home">Active</Nav.Link>
+        <Nav.Item style={{fontSize:"1.5rem"}}>
+          <Nav.Link href="/home">홈</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link>Link</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link>Link</Nav.Link>
+          <Nav.Link href="/product" style={{fontSize:"1.5rem"}}><FiShoppingCart/></Nav.Link>
         </Nav.Item>
       </Nav>
     );
--- src/components/Views/LandingPage/LandingPage.js
+++ src/components/Views/LandingPage/LandingPage.js
@@ -1,108 +1,120 @@
 import React, { useState, useEffect, useCallback } from "react";
 import styled from "styled-components";
 import CircleImage from "../../circleImage";
-import { Row, Col, Image,Card ,ProgressBar,OverlayTrigger} from "react-bootstrap";
+import {
+  Row,
+  Col,
+  Image,
+  Card,
+  ProgressBar,
+  OverlayTrigger
+} from "react-bootstrap";
 import axios from "axios";
+import { baseURL } from "../../config";
 
-const baseURL = "http://ddragon.leagueoflegends.com/cdn/10.4.1";
-
-    
 const Wrapper = styled.div`
   display: flex;
   border: 3px solid red;
-  flex-direction:row;
+  flex-direction: row;
   position: relative;
 `;
 const StyledImage = styled(Image)`
   cursor: pointer;
 `;
 
-
 const LandingPage = () => {
+  const [champion, setChampion] = useState("");
+  const [clickedImage, setClickedImage] = useState("");
+  const [SelecedChampions, setSelecedChampions] = useState([]);
 
-    const [champion, setChampion] = useState("");
-    const [clickedImage, setClickedImage] = useState("");
-    const [SelecedChampions, setSelecedChampions] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`${baseURL}/data/ko_KR/champion.json`)
+      .then(res => setChampion(res.data.data))
+      .catch(err => console.log(err));
+    axios.get("/hello").then(res => console.log(res));
+  }, []);
 
-    useEffect(() => {
-        axios
-          .get(`${baseURL}/data/ko_KR/champion.json`)
-          .then(res => setChampion(res.data.data))
-          .catch(err => console.log(err));
-      }, []);
-    
-      const realChampion = Object.keys(champion).map(cham => champion[cham]);
-      console.log(realChampion);
-    
-      const now = 100;
-    
-      const handleSelectChampion = useCallback((i) =>{
-        console.log(realChampion[i].image.full)
-        setClickedImage(() =>realChampion[i].image.full)
-        
-      },[realChampion])
-      console.log(clickedImage)
-      if(clickedImage !== "" && SelecedChampions.length <5 ){
-        SelecedChampions.push(clickedImage)
-      } 
-      console.log("선택된이미지들",SelecedChampions)
-      
-    return (
-        <Wrapper>
-        <div style={{width: "350%" ,border: "1px solid green"}}>
-        <CircleImage/>
-        </div>
-          <Row>
-          
-          <div style={{display:"flex",flexDirection:"column" ,position:"absolute" ,top:30,left:0 }}>
-          {SelecedChampions && SelecedChampions.map(SelecedChampion => <Image
-                    style={{width:"60px",height:"60px",margin:"120px 0 0 0"}}
-                    src={`${baseURL}/img/champion/${SelecedChampion}`}
-                    alt="SelecedChampion"
-                  />)}
-          </div>
-            {realChampion.map((cham,i) => (
-              <Col xs={6} sm={4} md={2} key={cham.key}>
-                <OverlayTrigger
-                  placement="bottom"
-                  overlay={
-                    <Card style={{ width: "18rem", padding: "1px" }}>
-                      <Card.Img
-                        variant="top"
-                        src={`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${cham.id}_0.jpg`}
-                      />
-                      <Card.Body>
-                        <Card.Title>{cham.name}</Card.Title>
-                        <Card.Text>{cham.title}</Card.Text>
-                        <Card.Text>
-                          <ProgressBar
-                            variant="success"
-                            now={now}
-                            label={`${now}%`}
-                          />
-                          {`HP:${cham.stats.hp}`}
-                          {`/MP:${cham.stats.mp}`}
-                        </Card.Text>
-                      </Card.Body>
-                    </Card>
-                  }
-                >
-                  <StyledImage
-                    roundedCircle
-                    src={`${baseURL}/img/champion/${cham.image.full}`}
-                    alt="champion"
-                    onClick={()=> handleSelectChampion(i)}
-                  />
-                 
-                </OverlayTrigger>
-                <p style={{ textAlign: "center", paddingRight: "17px" }}>
-                  {cham.name}
-                </p>
-              </Col>
+  const realChampion = Object.keys(champion).map(cham => champion[cham]);
+
+  const now = 100;
+
+  const handleSelectChampion = useCallback(
+    i => {
+      setClickedImage(() => realChampion[i].image.full);
+    },
+    [realChampion]
+  );
+
+  if (clickedImage !== "" && SelecedChampions.length < 5) {
+    SelecedChampions.push(clickedImage);
+  }
+
+  return (
+    <Wrapper>
+      <div style={{ width: "350%", border: "1px solid green" }}>
+        <CircleImage />
+      </div>
+      <Row>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            position: "absolute",
+            top: 30,
+            left: 0
+          }}
+        >
+          {SelecedChampions &&
+            SelecedChampions.map(SelecedChampion => (
+              <Image
+                style={{ width: "60px", height: "60px", margin: "120px 0 0 0" }}
+                src={`${baseURL}/img/champion/${SelecedChampion}`}
+                alt="SelecedChampion"
+              />
             ))}
-          </Row>
-        </Wrapper>
-    );
+        </div>
+        {realChampion.map((cham, i) => (
+          <Col xs={6} sm={4} md={2} key={cham.key}>
+            <OverlayTrigger
+              placement="bottom"
+              overlay={
+                <Card style={{ width: "18rem", padding: "1px" }}>
+                  <Card.Img
+                    variant="top"
+                    src={`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${cham.id}_0.jpg`}
+                  />
+                  <Card.Body>
+                    <Card.Title>{cham.name}</Card.Title>
+                    <Card.Text>{cham.title}</Card.Text>
+                    <Card.Text>
+                      <ProgressBar
+                        variant="success"
+                        now={now}
+                        label={`${now}%`}
+                      />
+                      {`HP:${cham.stats.hp}`}
+                      {`/MP:${cham.stats.mp}`}
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              }
+            >
+              <StyledImage
+                roundedCircle
+                src={`${baseURL}/img/champion/${cham.image.full}`}
+                alt="champion"
+                onClick={() => handleSelectChampion(i)}
+              />
+            </OverlayTrigger>
+            <p style={{ textAlign: "center", paddingRight: "17px" }}>
+              {cham.name}
+            </p>
+          </Col>
+        ))}
+      </Row>
+    </Wrapper>
+  );
 };
 
 export default LandingPage;
