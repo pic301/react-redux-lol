1dbfb5cf20e20224244a2e345ee260a893f805da
pic301 <ppic301@gmail.com>
2020-03-09 23:29:39 +0900

    loginPage redux 적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import styled from "styled-components";
 import FistImage from "./components/fistImage";
-import { Container, Image } from "react-bootstrap";
 import NavBar from "./components/NavBar";
-import LandingPage from "./components/Views/LandingPage/LandingPage";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// ==============================
+//           Pages
+// ==============================
+import LandingPage from "./components/Views/LandingPage/LandingPage";
+import ProductPage from "./components/Views/ProductPage/ProductPage";
+import LoginPage from "./components/Views/LoginPage/LoginPage";
+
+// ==============================
+//           Bootstrap
+// ==============================
+import { Container } from "react-bootstrap";
 
 const StyledContainer = styled(Container)`
   margin-bottom: 500px;
@@ -17,10 +27,23 @@ const App = () => {
     <Router>
       <div style={{ backgroundColor: "#333333" }}>
         <StyledContainer>
-          <NavBar />
-          <FistImage />
+          <Switch>
+            <Route path="/login" render={() => null} />
+            <Route path="/register" render={() => null} />
+            <Route
+              render={() => (
+                <>
+                  <NavBar />
+                  <FistImage />
+                </>
+              )}
+            />
+          </Switch>
+
           <Switch>
             <Route exact path="/" component={LandingPage} />
+            <Route exact path="/login" component={LoginPage} />
+            <Route exact path="/product" component={ProductPage} />
           </Switch>
         </StyledContainer>
       </div>
--- src/index.js
+++ src/index.js
@@ -4,13 +4,22 @@ import App from "./App";
 import { BrowserRouter } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.css";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
-import rootReducer from "./modules";
+import { createStore , applyMiddleware} from "redux";
+import rootReducer from "./reducers";
+import promiseMiddleware from 'redux-promise'
+import ReduxThunk from 'redux-thunk'
 
-const store = createStore(rootReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider
+  store={createStoreWithMiddleware(
+    rootReducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+  )}
+>
     <BrowserRouter>
     <App />
     </BrowserRouter>
