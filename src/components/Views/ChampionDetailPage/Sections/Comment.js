b4cd39c4fca989a7971b67c1fe3099aeaaea9834
pic301 <ppic301@gmail.com>
2020-03-14 23:05:01 +0900

    서버 모델생성하고 서버에 댓글들 저장하고 그 댓글 리스트들을 가져와서 보여주는 Comment 컴포넌트 그 댓글들 하나하나 single컴포넌트

--- src/components/Views/ChampionDetailPage/Sections/Comment.js
+++ src/components/Views/ChampionDetailPage/Sections/Comment.js
@@ -1,48 +1,82 @@
-import React ,{useState}from "react";
+import React, { useState, useEffect } from "react";
 import { Row, Form, Button } from "react-bootstrap";
 import Axios from "axios";
-import { useSelector } from 'react-redux'
+import { useSelector } from "react-redux";
+import SingleComment from "./SingleComment";
+import ReplyComment from "./ReplyComment";
 
-import SingleComment from './SingleComment'
+const Comment = ({ championId, commentLists, refresh }) => {
 
-const Comment = ({championId}) => {
-  const user = useSelector(state => state.user)
-  const [comment, setComment] = useState(' ')
+  const [commentValue, setCommentValue] = useState(" ");
+  const [loginUser,setLoginUser] = useState('')
 
 
-  const handleChange = (e) => {
-    setComment(e.target.value)
+
+  useEffect(()=>{
+    const user = localStorage.getItem('userId')
+    setLoginUser(user)
+
+  },[])
+  console.log("로그인 유저다다다다다다",loginUser)
+
+  const handleChange = e => {
+    setCommentValue(e.target.value);
   };
-  const onsubmit = (e) => {
-      e.preventDefault()
+  const onsubmit = e => {
+    e.preventDefault();
 
-      const variables ={
-          content:comment,
-          writer: user.userData._id,
-          postId:championId,
+    const variables = {
+      content: commentValue,
+      writer: loginUser,
+      championId: championId
+    };
+
+    Axios.post("/api/comment/saveComment", variables).then(res => {
+      if (res.data.success) {
+        console.log(res)
+       
+        // refresh(res.data.result);
+      }else{
+        alert('코멘트를 저장하는데 실패했습니다')
       }
-
-      Axios.post('api/comment/saveComment',variables).then(
-          res => console.log(res)
-      )
+    });
   };
   return (
     <div>
-
       {/* Comment Lists */}
-      <SingleComment />
+
+       {commentLists &&
+        commentLists.map(
+          (comment, i) =>
+            !comment.responseTo && (
+              <>
+                <SingleComment
+                  // refresh={refresh}
+                  comment={comment}
+                  championId={championId}
+                />
+                {/* <ReplyComment refresh={refresh} parentCommentId={comment._id} championId={championId}   commentLists={commentLists}/> */}
+              </>
+            )
+        )
+        } 
+         
       <Form style={{ display: "flex" }}>
         <Form.Group
           style={{ width: "100%" }}
           as={Row}
           controlId="formPlaintextPassword"
         >
-          <Form.Control onChange={handleChange} value={comment} as="textarea" rows="3" />
+          <Form.Control
+            onChange={handleChange}
+            value={commentValue}
+            as="textarea"
+            rows="3"
+          />
         </Form.Group>
-        <Button
-          onClick={onsubmit}
-          style={{ width: "20%", height: "52px" }}
-        >제출하기</Button>
+        <Button onClick={onsubmit} style={{ width: "20%", height: "52px" }}>
+          제출하기
+        </Button>
       </Form>
     </div>
   );
--- src/components/Views/ChampionDetailPage/Sections/SingleComment.js
+++ src/components/Views/ChampionDetailPage/Sections/SingleComment.js
@@ -1,39 +1,69 @@
-import React, {useState} from "react";
-import { Row, Form, Button,  } from "react-bootstrap";
+import React, { useState } from "react";
+import { Row, Form, Button } from "react-bootstrap";
+import { useSelector } from 'react-redux'
+import Axios from 'axios'
 
-const SingleComment = () => {
-  const [openReply,setOpenReply] = useState(false);
-  const [commentValue,setCommentValue] = useState()
+const SingleComment = ({championId,comment,refresh}) => {
+  const [openReply, setOpenReply] = useState(false);
+  const user = useSelector(state => state.user)
+  const [commentValue, setCommentValue] = useState();
 
-  const onChangeComment = (e) => {
-    setCommentValue(e.target.value)
-  }
+  const handleChange = e => {
+    setCommentValue(e.target.value);
+  };
+  const onSubmit = e => {
+    e.preventDefault();
 
+  //   const variables ={
+  //     content:commentValue,
+  //     writer: user.userId,
+  //     championId:championId,
+  //     responseTo: comment._id
+      
+  // }
+
+  //   Axios.post("api/comment/saveComment", variables).then(res =>{
+  //     if(res.data.success){
+  //       setCommentValue("")
+  //       refresh((res.data.result))
+  //     }else{
+  //       alert("코멘트 저장 실패")
+  //     }
+  //   });
+
+  };
+  console.log("comment",comment)
   return (
     <div style={{ color: "black" }}>
-      <Button onClick={() => setOpenReply(!openReply)}>답글달기</Button>
-     {openReply && 
-       <Form style={{ display: "flex" }}>
-       <Form.Group
-         style={{ width: "100%" }}
-         as={Row}
-         controlId="formPlaintextPassword"
-       >
-         <Form.Control
-           onChange={onChangeComment}
-           value={commentValue}
-           as="textarea"
-           rows="3"
-         />
-       </Form.Group>
-       <Button
-         onClick
-         style={{ width: "20%", height: "52px" }}
-       >제출하기</Button>
-     </Form>
-    }
+
+      <div style={{ border:"3px solid red"}}>
+        {comment.writer.name} 
+        {comment.content} 
+       <Button onClick={() => setOpenReply(!openReply)}>답글달기</Button>
+      </div>
+      {openReply && (
+        <Form style={{ display: "flex"  }} onSubmit={onSubmit}>
+          <Form.Group
+            style={{ width: "100%" }}
+            as={Row}
+            controlId="formPlaintextPassword"
+          >
+            <Form.Control
+              onChange={handleChange}
+              value={commentValue}
+              as="textarea"
+              rows="3"
+            />
+          </Form.Group>
+          <Button onClick={onSubmit} style={{ width: "20%", height: "52px" }}>
+            제출하기
+          </Button>
+        </Form>
+      )}
     </div>
   );
 };
 
 export default SingleComment;
+
+
