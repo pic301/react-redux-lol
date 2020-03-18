6692bb41309a6a0a88bf53ea74fed07afb99d4ee
pic301 <ppic301@gmail.com>
2020-03-18 22:59:57 +0900

    서버 리팩토링

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use('/api/user', userAPIRouter)
+app.use('/api/user', userAPIRouter )
 
 
 
