21edc3226c6a98fb9cc6bf5b41afdab70471b939
pic301 <ppic301@gmail.com>
2020-03-23 20:15:48 +0900

    상품 api를 호출해서 가져오고 보여주기

--- src/components/Views/ProductPage/ProductPage.js
+++ src/components/Views/ProductPage/ProductPage.js
@@ -1,13 +1,61 @@
-import React from 'react';
-
+import React, { useState, useEffect } from "react";
+import { Button, Card, Row,Col , Pagination} from "react-bootstrap";
+import Axios from "axios";
+const parser = require("fast-xml-parser");
 
 const ProductPage = () => {
   
+  const [products, setProducts] = useState("");
+  useEffect(() => {
+    Axios(
+      "https://cors-anywhere.herokuapp.com/http://openapi.11st.co.kr/openapi/OpenApiService.tmall?key=fc559c28fcef7aac74de4e9fe5a8897a&apiCode=ProductSearch&keyword=리그오브레전드피규어&pageNum=1"
+    ).then(res => {
+      const jsonObj = parser.parse(res.data);
+      console.log(jsonObj);
+      setProducts(jsonObj.ProductSearchResponse.Products.Product);
+    });
+  }, []);
+  console.log(products);
   return (
     <div>
+      <Row >
+      {products &&
+        products.map((product, i) => (
+          
+            <Col >
+              <Card>
+                <img
+                  width="300px"
+                  height="300px"
+                  src={`${product.ProductImage300}`}
+                  alt=""
+                />
+                <div style={{ color: "red" }}>{product.ProductName}</div>
+                <div style={{ color: "red" }}>{product.ProductPrice}원</div>
+                <div style={{ color: "red" }}>{product.Delivery}</div>
+              </Card>
+              <Button>장바구니담기</Button>
+              <Button>바로구매하기</Button>
+            </Col>
+        ))}
+        </Row>
 
+        <nav aria-label="Page navigation example">
+          <ul class="pagination">
+            <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item"><a class="page-link" href="#">Next</a></li>
+          </ul>
+        </nav>
     </div>
+
+    
   );
+  
 };
 
 export default ProductPage;
+
+
