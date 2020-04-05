2fcaa40ed2da91c7cfde81af6a84afc907090b52
pic301 <ppic301@gmail.com>
2020-04-05 19:28:37 +0900

    루트 리듀서에 champion 추가

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,12 +4,14 @@ import user from './user_reducer'
 import summoners from './summoners_reducer'
 import favorite from './favorite_reducer'
 import products from './products_reducer'
+import champion from './champion_reducer'
 
 const rootReducer = combineReducers({
     user,
     summoners,
     favorite,
     products,
+    champion,
 });
 
 export default rootReducer
