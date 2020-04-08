b7e5da1f8446450eb9b720befb6a0c7b0838b464
pic301 <ppic301@gmail.com>
2020-04-08 22:45:27 +0900

    받아온 product 정보를 화면에 표시, reduce 이용해서 합계

--- src/components/Views/CartPage/CartPage.js
+++ src/components/Views/CartPage/CartPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
+import {Table} from 'react-bootstrap'
 
 const CartPage = () => {
     const [cartProducts,setCartProducts] = useState('')
@@ -17,11 +18,37 @@ const CartPage = () => {
           }
         });
     },[])
-    console.log(cartProducts)
+   const prices = cartProducts && cartProducts.map((product,i) => cartProducts[i].product.ProductPrice)
+   const totalSum = prices && Array.from(prices).reduce((acc,cur) => acc+cur)
+  
+   console.log(cartProducts)
+
     return (
-        <div style={{color:'red'}}>
-          {cartProducts&& cartProducts.map((product,i) =><div>{product.id}</div>) }
-        </div>
+        <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>상품/옵션정보</th>
+            <th>수량</th>
+            <th>가격</th>
+            <th>배송비</th>
+            <th>판매자</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cartProducts && cartProducts.map((product,i) =>
+          <tr key={product.date}>
+            <td><img src={`${product.product.ProductImage100}`} alt=""/>{product.product.ProductName}</td>
+            <td>{product.quantity}</td>
+            <td>{product.product.ProductPrice}</td>
+            <td>{product.product.Delivery}</td>
+            <td>{product.product.Seller}</td>
+          </tr>
+                   )}
+        <tr>
+          <td>주문금액:{totalSum}원</td>
+        </tr>
+        </tbody>
+      </Table>
     );
 };
 
