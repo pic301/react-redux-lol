9ac6aa9a3bf3b91cb3b66986ad0e7bdc394c2bac
pic301 <ppic301@gmail.com>
2020-04-08 22:28:56 +0900

    장바구니에 담긴 카트정보를 가져오기

--- src/components/Views/CartPage/CartPage.js
+++ src/components/Views/CartPage/CartPage.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 
 const CartPage = () => {
+    const [cartProducts,setCartProducts] = useState('')
+
+    useEffect(() =>{
+        axios
+        .post("/api/users/cart", {
+          userFrom: localStorage.getItem("userId")
+        })
+        .then(res => {
+          try {
+            setCartProducts(res.data.doc[0].cart);
+          } catch (error) {
+            console.log(error);
+          }
+        });
+    },[])
+    console.log(cartProducts)
     return (
-        <div>
-            
+        <div style={{color:'red'}}>
+          {cartProducts&& cartProducts.map((product,i) =><div>{product.id}</div>) }
         </div>
     );
 };
