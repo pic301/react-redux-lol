5e58920f7e885d89caa94a597586e56c7a265a24
pic301 <ppic301@gmail.com>
2020-04-09 15:57:57 +0900

    initialize heroku

---
+++ src/components/config.js
@@ -0,0 +1,6 @@
+
+export const baseURL = "http://ddragon.leagueoflegends.com/cdn/10.5.1";
+export const imgURL = "http://ddragon.leagueoflegends.com/cdn/img";
+export const API_KEY = "RGAPI-57a5c276-def0-48e1-8460-d8367b8a9278";
+export const API_KEY_SHOP = "fc559c28fcef7aac74de4e9fe5a8897a";
+export const CORS_ANYWHERE = "https://cors-anywhere.herokuapp.com/";
