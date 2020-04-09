b5caad9a4e684bcb54b3a334fc3f8f64888cbd03
pic301 <ppic301@gmail.com>
2020-04-09 12:45:24 +0900

    RegisterPage 꾸미기

--- src/components/Views/RegisterPage/RegisterPage.js
+++ src/components/Views/RegisterPage/RegisterPage.js
@@ -1,6 +1,75 @@
 import React,{ useState } from "react";
 import { useDispatch } from 'react-redux'
 import { registerUser } from '../../../actions/user_actions'
+import styled from 'styled-components'
+
+
+const StyledWrapper = styled.div`
+    display:flex;
+    align-items:center;
+    height:100vh;
+`;
+
+const Container = styled.div`
+  position: relative;
+  background-color: #FFFFFF;
+  width: 400px;
+  height: 600px;
+  margin: 1rem auto;
+  border-radius: 1.5em;
+  box-shadow: 0px 11px 35px 2px rgba(0, 0, 0, 0.14);
+  & > form{
+    padding-top: 40px;
+  }
+  @media (max-width: 600px) {
+  .Container {
+      border-radius: 0px;
+  }
+}
+`;
+const RegisterTitle = styled.p`
+  color: #8C55AA;
+  font-weight: bold;
+  font-size: 23px;
+  padding-top: 40px;
+  text-align:center;
+`;
+const StyledInput = styled.input`
+width: 76%;
+color: rgb(38, 50, 56);
+font-weight: 700;
+font-size: 14px;
+letter-spacing: 1px;
+background: rgba(136, 126, 126, 0.04);
+padding: 10px 20px;
+border: none;
+border-radius: 20px;
+outline: none;
+box-sizing: border-box;
+border: 2px solid rgba(0, 0, 0, 0.02);
+margin-bottom: 50px;
+margin-left: 46px;
+text-align: center;
+margin-bottom: 27px;
+font-family: 'Ubuntu', sans-serif;
+`;
+const RegisterButton = styled.button`
+    cursor: pointer;
+    border-radius: 5em;
+    color: #fff;
+    background: linear-gradient(to right, #9C27B0, #E040FB);
+    border: 0;
+    padding-left: 40px;
+    padding-right: 40px;
+    padding-bottom: 10px;
+    padding-top: 10px;
+    font-family: 'Ubuntu', sans-serif;
+    margin-left: 35%;
+    font-size: 13px;
+    box-shadow: 0 0 20px 1px rgba(0, 0, 0, 0.04);
+    text-align:center;
+`;
+
 
 const RegisterPage = () => { 
 
@@ -34,23 +103,44 @@ const onChangeName = (e) =>{
     console.log(dataToSubmit)
     dispatch(registerUser(dataToSubmit)).then(res =>{
         console.log(res)
-        // if(res.payload.success){
-
-        // }else{
-
-        // }
         }) 
   } 
   return( 
   <div>
-      <form >
-      <input type="text" onChange={onChangeName} value={name} />
-      <input type="email" onChange={onChangeEmail} value={email} />
-      <input type="password" onChange={onChangePassword} value={password} />
-      <input type="password" onChange={onChangePasswordCheck} value={passwordCheck} />
-      <button onClick={subminForm}></button>
-      </form>
-      
+    <StyledWrapper >
+      <Container>
+        <RegisterTitle class="sign">회원가입</RegisterTitle>
+        <form>
+          <StyledInput
+            type="text"
+            onChange={onChangeName}
+            value={name}
+            placeholder="Name"
+          />
+            <StyledInput
+              type="email"
+              onChange={onChangeEmail}
+              value={email}
+              placeholder="UserEmail"
+            />
+          <StyledInput
+            type="password"
+            onChange={onChangePassword}
+            value={password}
+            placeholder="Password"
+          />
+          <StyledInput
+            type="password"
+            onChange={onChangePasswordCheck}
+            value={passwordCheck}
+            placeholder="PasswordCheck"
+          />
+          <RegisterButton type="submit" name="action" onClick={subminForm}>
+            등록
+          </RegisterButton>
+        </form>
+      </Container>
+    </StyledWrapper>
   </div>
   )
 };
