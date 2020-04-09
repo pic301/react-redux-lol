c2dd7978ae47d78448f83ec36166cb8e0759467b
pic301 <ppic301@gmail.com>
2020-04-09 12:44:20 +0900

    detailPage css 꾸미기

--- src/components/Views/ChampionDetailPage/ChampionDetailPage.js
+++ src/components/Views/ChampionDetailPage/ChampionDetailPage.js
@@ -1,60 +1,131 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { baseURL, imgURL } from "../../config";
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 import Favorite from "./Sections/Favorite";
 import Comment from "./Sections/Comment";
-
 import Carousel from "react-bootstrap/Carousel";
+import { palette } from "../../../lib/styles/palette";
 
+
+const TextStyle = css`
+  color: #fff;
+  font-size: 2rem;
+  font-weight:bold;
+  background: linear-gradient(to right, #cc5de8 10%, #fff 50%, #4dabf7 60%);
+  background-size: 200% auto;
+  color: #fff;
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  animation: textclip 3.5s linear infinite;
+  display: inline-block;
+  @keyframes textclip {
+    to {
+      background-position: 200% center;
+    }
+  }
+`;
+
+const DetailContainer = styled.div`
+  @import url('https://fonts.googleapis.com/css?family=Nanum+Gothic+Coding&display=swap');
+  background-color:#ffffff;
+  color:${palette.gray[8]}; 
+  font-size: 1.1rem;
+  font-family: 'Nanum Gothic Coding', monospace;
+  line-height:1.5;
+  padding:0 10px;
+`;
 const StyledImage = styled.img`
   width: 100%;
   height: 400px;
 `;
 const SpellContainer = styled.div`
   display: flex;
-  border: 2px solid green;
+  background-color:${palette.gray[2]};
+  height:200px;
+  
 `;
 const SpellPassive = styled.div`
-  margin-right: 20px;
+  display: flex;
+  background-color:${palette.gray[2]};
 `;
-
 const Spell = styled.div`
   margin-right: 20px;
   text-align: center;
+  & > img {
+    width:70px;
+    border:3px solid black;
+    cursor: pointer;
+  }
+`;
+const SpellName = styled.div`
+  font-size: 1rem;
+`;
+const StyledSkinName = styled.div`
+  text-align: center;
+  font-size: 1.5rem;
+  margin-top: 10px;
+`;
+const PassiveName = styled.div`
+`;
+const PassiveDescription = styled.div`
+    margin:10px;
+`;
+const DetailChampionContainer = styled.div`
+    text-align:center;
+    margin:10px;
+    padding:10px;
+    
+`;
+const DetailChampionName = styled.div`
+   margin:10px;
+   ${TextStyle}
+`;
+const PassiveTitle = styled.div`
+   ${TextStyle}
+   font-size:2rem;
+`;
+const SkillTilte = styled.div`
+   ${TextStyle}
+   font-size:2rem;
+`;
+const DetailChampionDescripion = styled.div`
+  background-color:${palette.gray[2]};
+  padding: 20px;
 `;
 
 const ChampionDetailPage = ({ match }) => {
   const [detailChampionSkins, setDetailChampionSkins] = useState([]);
   const [detailChampion, setDetailChampion] = useState("");
   const [spellName, setSpellName] = useState("");
-  const [spellDescription, setSpellDescription] = useState("");
+  const [spellDescription, setSpellDescription] = useState('');
   const [comments, setComments] = useState([]);
   const { championId } = match.params;
-  const variables = {championId:championId}
-  
+
+  const variables = { championId: championId };
+
   useEffect(() => {
     axios.get(`${baseURL}/data/ko_KR/champion/${championId}.json`).then(res => {
+      setSpellDescription(res.data.data[championId].spells[0].description)
+      setSpellName(res.data.data[championId].spells[0].name)
       setDetailChampion(res.data.data[championId]);
       setDetailChampionSkins(res.data.data[championId].skins);
     });
-    
-    
-    axios.post("/api/comment/getComments", variables).then(res =>{
-      if(res.data.success){
-        console.log(res)
-        setComments(res.data.comments)
-      }else{
-        alert("댓글 가져오는데 실패")
+
+    axios.post("/api/comment/getComments", variables).then(res => {
+      if (res.data.success) {
+        console.log(res);
+        setComments(res.data.comments);
+      } else {
+        alert("댓글 가져오는데 실패");
       }
-      
     });
   }, []);
-  
-  console.log(comments)
-  const refresh = (newComment) =>{
-    setComments(comments.concat(newComment))
-  }
+
+  const refresh = newComment => {
+    setComments(comments.concat(newComment));
+  };
 
   const { spells } = detailChampion;
 
@@ -63,7 +134,7 @@ const ChampionDetailPage = ({ match }) => {
     setSpellDescription(spells[i].description);
   };
   return (
-    <>
+    <DetailContainer >
       <Carousel
         controls={true}
         indicators={false}
@@ -78,58 +149,83 @@ const ChampionDetailPage = ({ match }) => {
                 src={`${imgURL}/champion/splash/${detailChampion.id}_${detailChampionSkins[i].num}.jpg`}
                 alt=""
               />
-              <div style={{ color: "red" }}>{detailChampionSkins[i].name}</div>
+              {detailChampionSkins[i].name === "default" ? (
+                <StyledSkinName>{"기본스킨"}</StyledSkinName>
+              ) : (
+                <StyledSkinName>{detailChampionSkins[i].name}</StyledSkinName>
+              )}
             </Carousel.Item>
           ))}
       </Carousel>
-      <div >
+      <div>
         <Favorite
           championId={championId}
           userFrom={localStorage.getItem("userId")}
         />
       </div>
+     <div style={{backgroundColor:`${palette.gray[3]}`}}>
+     <div style={{textAlign:"center" }}>
+      <PassiveTitle>패시브</PassiveTitle>
+      </div>
       <SpellPassive>
         {detailChampion && (
           <>
-            <img
-              src={`http://ddragon.leagueoflegends.com/cdn/10.5.1/img/passive/${detailChampion.passive.image.full}`}
-              alt=""
-            />
-            <div style={{ color: "black" }}>{detailChampion.passive.name}</div>
-            <div style={{ color: "black" }}>
-              {detailChampion.passive.description}
+            <div style={{ display: "flex",  justifyContent: "center" ,width:"25%" }}>
+              <div style={{display:"flex",flexDirection:'column' ,alignItems:"center"}}>
+              <img
+                src={`http://ddragon.leagueoflegends.com/cdn/10.5.1/img/passive/${detailChampion.passive.image.full}`}
+                alt=""
+              />
+              <PassiveName >{detailChampion.passive.name}</PassiveName>
+              </div>
+            </div>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <PassiveDescription>
+                {detailChampion.passive.description.replace(/<(\/)?([a-zA-Z]*)(\s[a-zA-Z]*=[^>]*)?(\s)*(\/)?>/ig, "")}
+              </PassiveDescription>
             </div>
           </>
         )}
       </SpellPassive>
-      <SpellContainer>
-        {spells &&
+      <div style={{textAlign:"center"}}>
+      <SkillTilte >스킬</SkillTilte>
+      </div>
+      <SpellContainer >
+          <div style={{display:"flex", width:"50%",}}>
+          {spells &&
           Object.keys(spells).map((spell, i) => (
-            <Spell key={spells[i].id}>
+            <div style={{display:"flex", alignItems:"center"}}>
+                <Spell key={spells[i].id} >
               <img
                 src={`http://ddragon.leagueoflegends.com/cdn/10.5.1/img/spell/${spells[i].image.full}`}
                 alt=""
                 onClick={() => _onClick(i)}
               />
-              <div style={{ color: "black" }}>{spells[i].name}</div>
+              <SpellName>{spells[i].name}</SpellName>
             </Spell>
+            </div>
           ))}
-        {}
-        <div style={{ color: "red", width: "40%" }}>
-          <div>{spellName}</div>
-          <div>{spellDescription}</div>
+          </div>
+        <div style={{ width:"50%" }}>
+          <div style={{ textAlign: "center", margin: "10px", fontSize:"1.5rem" }}>{spellName}</div>
+          <div >{spellDescription.replace(/<(\/)?([a-zA-Z]*)(\s[a-zA-Z]*=[^>]*)?(\s)*(\/)?>/ig, "")}</div>
         </div>
       </SpellContainer>
-      <h2 style={{ color: "black" }}>
-        {detailChampion.name}
-        <span>{detailChampion.tags}</span>
-      </h2>
-      <div style={{ color: "black" }}>{detailChampion.lore}</div>
-
+        <DetailChampionContainer>
+          <img src={`http://ddragon.leagueoflegends.com/cdn/10.6.1/img/champion/${championId}.png`} alt=""/>
+          <DetailChampionName>챔피언:{detailChampion.name}</DetailChampionName>
+          <span>역할군:{detailChampion.tags}</span>
+        <DetailChampionDescripion>{detailChampion.lore}</DetailChampionDescripion>
+        </DetailChampionContainer>
+     
+     </div>
       {/* 서버에서 온 댓글리스트들 배열을 props로 Comment 에 넘겨준다 */}
-      <Comment refresh={refresh} commentLists={comments} championId={championId}/>
-    
-    </>
+      <Comment
+        refresh={refresh}
+        commentLists={comments}
+        championId={championId}
+      />
+    </DetailContainer>
   );
 };
 export default ChampionDetailPage;
