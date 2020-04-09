b0c890e3c12cb9a4ab4b69f9997541f728c29353
pic301 <ppic301@gmail.com>
2020-04-09 12:28:01 +0900

    Cart page 결제 버튼 완성

--- src/components/Views/CartPage/CartPage.js
+++ src/components/Views/CartPage/CartPage.js
@@ -1,7 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
-import {Table} from 'react-bootstrap'
+import { Table } from 'react-bootstrap'
+import styled from 'styled-components'
+import Button from '../../common/Button'
+import { palette } from '../../../lib/styles/palette'
 
+const TableContainer = styled.div`
+    background-color:#ffffff;
+    font-size: 1rem;
+`;
+const StyledTd = styled.td`
+    background-color:#ffffff;
+    font-size: 2rem;
+    font-weight: bold;
+    text-align:center;
+`;
+const PaymentContainer = styled.div`
+    display:flex;
+    align-items:center;
+    justify-content:space-between;
+`;
+const PaymentButton = styled(Button)`
+    background-color:${palette.red[6]};
+    &:hover{
+      background-color:${palette.red[8]};
+    }
+    
+`;
 const CartPage = () => {
     const [cartProducts,setCartProducts] = useState('')
 
@@ -10,8 +35,7 @@ const CartPage = () => {
     },[])
     
    const prices = cartProducts && cartProducts.map((product,i) => cartProducts[i].product.ProductPrice)
-   const totalSum = prices && Array.from(prices).reduce((acc,cur) => acc+cur)
-  
+   const totalSum = prices && Array.from(prices).reduce((acc,cur) => acc+cur,0)
    console.log(cartProducts)
 
    const onRemoveCartProduct =(product) =>{
@@ -42,13 +66,14 @@ const CartPage = () => {
           }
         });
   };
-
-
+  const onPayment = () =>{
+    alert('아직 준비중입니다.')
+  }
     return (
-    <>
-        <Table striped bordered hover>
+    <TableContainer>
+        <Table striped bordered hover  >
         <thead>
-          <tr>
+          <tr style={{textAlign:"center"}}>
             <th>상품/옵션정보</th>
             <th>수량</th>
             <th>가격</th>
@@ -56,24 +81,26 @@ const CartPage = () => {
             <th>판매자</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody style={{lineHeight:"6"}}>
           {cartProducts && cartProducts.map((product,i) =>
           <tr key={product.date}>
             <td><img src={`${product.product.ProductImage100}`} alt="ProductImage"/>{product.product.ProductName}</td>
             <td>{product.quantity}</td>
-            <td>{product.product.ProductPrice}</td>
+            <td>{product.product.ProductPrice}원</td>
             <td>{product.product.Delivery}</td>
             <td>{product.product.Seller}</td>
-            <td><button onClick={() =>onRemoveCartProduct(product)}>삭제하기</button></td>
+            <td><Button onClick={() =>onRemoveCartProduct(product)}>삭제하기</Button></td>
           </tr>
-                   )}
-        <tr>
-          <td>주문금액:{totalSum}원</td>
-        </tr>
+          )}
+          <PaymentContainer >  
+            <StyledTd>
+              주문금액:{totalSum}원
+            </StyledTd>
+            <PaymentButton onClick={onPayment}>결제하기</PaymentButton>
+          </PaymentContainer>
         </tbody>
       </Table>
-      <button>결제하기</button>
-    </>
+    </TableContainer>
     );
 };
 
