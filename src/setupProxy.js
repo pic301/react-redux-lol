3ba89f5c788806f7e0338cc5a62056c10758d7b7
pic301 <ppic301@gmail.com>
2020-04-10 13:51:44 +0900

    Proxy 문제

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,8 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
     createProxyMiddleware('/api', {
-      target: "http://localhost:5000/",
-      secure: false,
+      target: "https://powerful-falls-31436.herokuapp.com/",
       changeOrigin: true
     })
   );
