a384f7f25a40b3b88a7816fbbe226d394bf0ede5
pic301 <ppic301@gmail.com>
2020-04-10 13:31:24 +0900

    Proxy 문제

--- server/index.js
+++ server/index.js
@@ -10,14 +10,9 @@ const { Comment } = require("./models/Comment");
 const { User } = require("./models/User");
 const { auth } = require('./middleware/auth')
 
-const cors = require('cors')
-
-app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-
-app.options('*', cors())
 
 
 const mongoose = require("mongoose");
@@ -239,8 +234,6 @@ app.post('/api/comment/getComments', (req, res) => {
       })
 })
 
-
- 
 
 const port = process.env.PORT || 5000; //백엔드 서버
 app.listen(port, () => console.log(`${port}`));
